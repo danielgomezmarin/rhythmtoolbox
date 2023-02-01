ebfeaf391a007b7f0b0b6ada6b5aacc8987cf874
p3zo <p3zo@users.noreply.github.com>
2023-02-01 00:02:37 -0800

    Parse low/mid/hi streams once and pass to descriptor fxs
    - Merge low/mid/hi functions
    - This avoids at least 15 redundant calls to get_stream for a significant performance increase

--- rhythmtoolbox/__init__.py
+++ rhythmtoolbox/__init__.py
@@ -1,25 +1,48 @@
 import numpy as np
 
 from .descriptors import (
-    hiD,
-    hiness,
-    hisync,
-    hisyness,
-    lowD,
-    lowness,
-    lowsync,
-    lowsyness,
-    midD,
-    midness,
-    midsync,
-    midsyness,
+    bandness,
+    density,
+    get_n_onset_steps,
     noi,
     polybalance,
     polyD,
     polyevenness,
     polysync,
     stepD,
+    syncopation16,
+    syness,
 )
+from .midi_mapping import hi_instruments, low_instruments, mid_instruments
+
+
+def get_stream(roll, band="low"):
+    """Returns a monophonic onset pattern of instruments in the given frequency band.
+
+    roll, np.array
+        Piano roll
+
+    band, str
+        "low", "mid", or "hi"
+    """
+    range_map = {
+        "low": low_instruments,
+        "mid": mid_instruments,
+        "hi": hi_instruments,
+    }
+
+    if band not in range_map:
+        raise ValueError(f"Invalid band `{band}`. Must be low, mid, or hi")
+
+    return ((roll[:, range_map[band]]).sum(axis=1) > 0).astype(int)
+
+
+def get_streams(roll):
+    return (
+        get_stream(roll, band="low"),
+        get_stream(roll, band="mid"),
+        get_stream(roll, band="hi"),
+    )
 
 
 def pattlist_to_pianoroll(pattlist):
@@ -34,42 +57,59 @@ def pianoroll2descriptors(roll):
 
     Some descriptors are valid only for 16-step patterns and will be None if the pattern is not divisible by 16.
     """
-    descriptors = {
-        "noi": noi,
-        "lowD": lowD,
-        "midD": midD,
-        "hiD": hiD,
-        "stepD": stepD,
-        "lowness": lowness,
-        "midness": midness,
-        "hiness": hiness,
-        "polyD": polyD,
-    }
+    descriptor_names = [
+        "noi",
+        "lowD",
+        "midD",
+        "hiD",
+        "stepD",
+        "lowness",
+        "midness",
+        "hiness",
+        "lowsync",
+        "midsync",
+        "hisync",
+        "lowsyness",
+        "midsyness",
+        "hisyness",
+        "polybalance",
+        "polyevenness",
+        "polysync",
+        "polyD",
+    ]
 
-    sixteen_step_descriptors = {
-        "lowsync": lowsync,
-        "midsync": midsync,
-        "hisync": hisync,
-        "lowsyness": lowsyness,
-        "midsyness": midsyness,
-        "hisyness": hisyness,
-        "polysync": polysync,
-        "polyevenness": polyevenness,
-        "polybalance": polybalance,
-    }
+    # Initialize the return dict
+    result = {d: None for d in descriptor_names}
 
-    result = {}
-    for key, func in descriptors.items():
-        result[key] = func(roll)
+    # No need to compute descriptors for empty patterns
+    n_onset_steps = get_n_onset_steps(roll)
+    if n_onset_steps == 0:
+        return result
 
-    for key, func in sixteen_step_descriptors.items():
-        result[key] = None
-        if len(roll) % 16 == 0:
-            # Compute the descriptors for each 16-step subpattern
-            vals = []
-            for i in range(len(roll) - 16 + 1):
-                vals.append(func(roll[i : i + 16]))
-            result[key] = np.mean(vals)
+    # Get the onset pattern of each instrument band
+    lowstream, midstream, histream = get_streams(roll)
+
+    result["noi"] = noi(roll)
+    result["lowD"] = density(lowstream)
+    result["midD"] = density(midstream)
+    result["hiD"] = density(histream)
+    result["stepD"] = stepD(roll)
+    result["lowness"] = bandness(lowstream, n_onset_steps)
+    result["midness"] = bandness(midstream, n_onset_steps)
+    result["hiness"] = bandness(histream, n_onset_steps)
+
+    # Compute descriptors that are valid only for 16-step patterns
+    if len(roll) == 16:
+        result["lowsync"] = syncopation16(lowstream)
+        result["midsync"] = syncopation16(midstream)
+        result["hisync"] = syncopation16(histream)
+        result["lowsyness"] = syness(lowstream)
+        result["midsyness"] = syness(midstream)
+        result["hisyness"] = syness(histream)
+        result["polybalance"] = polybalance(lowstream, midstream, histream)
+        result["polyevenness"] = polyevenness(lowstream, midstream, histream)
+        result["polysync"] = polysync(lowstream, midstream, histream)
+        result["polyD"] = polyD(lowstream, midstream, histream)
 
     return result
 
@@ -82,6 +122,5 @@ def pattlist2descriptors(pattlist):
 
     Some descriptors are valid only for 16-step patterns and will be None if the pattern is not divisible by 16.
     """
-
     roll = pattlist_to_pianoroll(pattlist)
     return pianoroll2descriptors(roll)
