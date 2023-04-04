61874e9918a792ca06e220e8835dae44020d157e
p3zo <p3zo@users.noreply.github.com>
2023-04-03 21:40:19 -0700

    Add test for non-drum patterns

--- tests/test_init.py
+++ tests/test_init.py
@@ -96,46 +96,130 @@ def test_pianoroll2descriptors():
     assert v["polyEvenness"] == 5.2753683906977775
     assert v["polyBalance"] == 0.9618538544571633
     assert v["polyDensity"] == 19
+    assert v["syness"] == -0.6363636363636364
+    assert v["balance"] == 0.8855199884772941
+    assert v["evenness"] == 0.9649989736702275
+    assert v["sync"] == -7
+
+    v = pianoroll2descriptors(BOSKA_3, drums=False)
+    assert v["noi"] == 5
+    assert v["stepDensity"] == 0.6875
+    assert v["polyDensity"] == 11
+    assert v["sync"] == 0
+    assert v["balance"] == 0.8855199884772941
+    assert v["evenness"] == 0.9649989736702275
+    assert v["syness"] == -0.6363636363636364
+    assert v["lowDensity"] is None
+    assert v["midDensity"] is None
+    assert v["hiDensity"] is None
+    assert v["lowness"] is None
+    assert v["midness"] is None
+    assert v["hiness"] is None
+    assert v["lowSync"] is None
+    assert v["midSync"] is None
+    assert v["hiSync"] is None
+    assert v["polySync"] is None
+    assert v["lowSyness"] is None
+    assert v["midSyness"] is None
+    assert v["hiSyness"] is None
+    assert v["polyBalance"] is None
+    assert v["polyEvenness"] is None
 
     v = pianoroll2descriptors(BOSKA_8)
     assert v["noi"] == 4
+    assert v["stepDensity"] == 1.0
     assert v["lowDensity"] == 6
     assert v["midDensity"] == 10
     assert v["hiDensity"] == 9
-    assert v["stepDensity"] == 1.0
+    assert v["polyDensity"] == 25
     assert v["lowness"] == 0.375
     assert v["midness"] == 0.625
     assert v["hiness"] == 0.5625
+    assert v["sync"] == 0
     assert v["lowSync"] == 3
     assert v["midSync"] == -4
     assert v["hiSync"] == -5
+    assert v["polySync"] == 7
     assert v["lowSyness"] == 0.5
     assert v["midSyness"] == -0.4
     assert v["hiSyness"] == -0.5555555555555556
-    assert v["polySync"] == 7
-    assert v["polyEvenness"] == 4.428794764473658
+    assert v["balance"] == 0.9999999999999999
     assert v["polyBalance"] == 0.9961791398488665
-    assert v["polyDensity"] == 25
+    assert v["evenness"] == 1.0
+    assert v["polyEvenness"] == 4.428794764473658
+    assert v["syness"] == 0.0
+
+    v = pianoroll2descriptors(BOSKA_8, drums=False)
+    assert v["noi"] == 4
+    assert v["stepDensity"] == 1.0
+    assert v["polyDensity"] == 16
+    assert v["sync"] == 0
+    assert v["balance"] == 0.9999999999999999
+    assert v["evenness"] == 1.0
+    assert v["syness"] == 0.0
+    assert v["lowDensity"] is None
+    assert v["midDensity"] is None
+    assert v["hiDensity"] is None
+    assert v["lowness"] is None
+    assert v["midness"] is None
+    assert v["hiness"] is None
+    assert v["lowSync"] is None
+    assert v["midSync"] is None
+    assert v["hiSync"] is None
+    assert v["polySync"] is None
+    assert v["lowSyness"] is None
+    assert v["midSyness"] is None
+    assert v["hiSyness"] is None
+    assert v["polyBalance"] is None
+    assert v["polyEvenness"] is None
 
     v = pianoroll2descriptors(BOSKA_9)
     assert v["noi"] == 6
+    assert v["stepDensity"] == 0.875
     assert v["lowDensity"] == 8
     assert v["midDensity"] == 7
     assert v["hiDensity"] == 9
-    assert v["stepDensity"] == 0.875
+    assert v["polyDensity"] == 24
     assert v["lowness"] == 0.5714285714285714
     assert v["midness"] == 0.5
     assert v["hiness"] == 0.6428571428571429
+    assert v["sync"] == -3
     assert v["lowSync"] == 4
     assert v["midSync"] == 2
     assert v["hiSync"] == -12
+    assert v["polySync"] == 20
     assert v["lowSyness"] == 0.5
     assert v["midSyness"] == 0.2857142857142857
     assert v["hiSyness"] == -1.3333333333333333
-    assert v["polySync"] == 20
-    assert v["polyEvenness"] == 4.9758868520193955
+    assert v["balance"] == 0.8680172096412447
     assert v["polyBalance"] == 0.7964353630870814
-    assert v["polyDensity"] == 24
+    assert v["evenness"] == 0.9551143976077295
+    assert v["polyEvenness"] == 4.9758868520193955
+    assert v["syness"] == -0.21428571428571427
+
+    v = pianoroll2descriptors(BOSKA_9, drums=False)
+    assert v["noi"] == 6
+    assert v["stepDensity"] == 0.875
+    assert v["polyDensity"] == 14
+    assert v["sync"] == 0
+    assert v["balance"] == 0.8680172096412447
+    assert v["evenness"] == 0.9551143976077295
+    assert v["syness"] == -0.21428571428571427
+    assert v["lowDensity"] is None
+    assert v["midDensity"] is None
+    assert v["hiDensity"] is None
+    assert v["lowness"] is None
+    assert v["midness"] is None
+    assert v["hiness"] is None
+    assert v["lowSync"] is None
+    assert v["midSync"] is None
+    assert v["hiSync"] is None
+    assert v["polySync"] is None
+    assert v["lowSyness"] is None
+    assert v["midSyness"] is None
+    assert v["hiSyness"] is None
+    assert v["polyBalance"] is None
+    assert v["polyEvenness"] is None
 
 
 def test_pattlist2descriptors():
